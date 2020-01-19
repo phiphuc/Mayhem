389e5caa2d619083cf33d23758c843ae92476007
donqu <itsol>
2020-01-20 02:18:26 +0700

    first commit

---
+++ Backend_Graphql/src/main/java/seabank/edu/EcommerceGraphqlApiApplication.java
@@ -0,0 +1,13 @@
+package seabank.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceGraphqlApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceGraphqlApiApplication.class, args);
+    }
+
+}
---
+++ Backend_Graphql/src/test/java/seabank/edu/EcommerceGraphqlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package seabank.edu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceGraphqlApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
